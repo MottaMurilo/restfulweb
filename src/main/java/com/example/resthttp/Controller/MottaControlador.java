fba2b96c5a6c88f867359619e4f812c9b047951d
Murilo Guizelin <murilomotta4@gmail.com>
2023-06-04 00:16:25 -0400

    /motta

--- src/main/java/com/example/resthttp/Controller/MottaControlador.java
+++ src/main/java/com/example/resthttp/Controller/MottaControlador.java
@@ -1,6 +1,7 @@
 package com.example.resthttp.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.resthttp.Entity.MottaEntidade;
@@ -8,9 +9,11 @@ import com.example.resthttp.Entity.MottaEntidade;
 @RestController
 public class MottaControlador {
 
+    private Long counter = 0L;
     @RequestMapping("/motta")
-    public MottaEntidade motta(){
-        return new MottaEntidade("murilo", 1L, 18);
-
+    public MottaEntidade motta(@RequestParam(value = "name") String name){ 
+        counter++;
+        return new MottaEntidade(name, counter, 18);
+            
 }
 }
